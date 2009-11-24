6c78a172c51086895cd1ea0028d17eb9a6a1b224
alexleutgoeb <privat@alexleutgoeb.com>
2009-11-24 18:30:44 +0100

    Added userinfo parser class.

---
+++ src/TDUserInfoParser.m
@@ -0,0 +1,41 @@
+//
+//  TDUserInfoParser.m
+//  ToodledoAPI
+//
+//  Created by Alex Leutgöb on 24.11.09.
+//  Copyright 2009 alexleutgoeb.com. All rights reserved.
+//
+
+#import "TDUserInfoParser.h"
+
+
+@implementation TDUserInfoParser
+
+- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qualifiedName attributes:(NSDictionary *)attributeDict {
+	if ([elementName isEqualToString:@"account"]) {
+		userInfos = [[NSDictionary alloc] init];
+		[results addObject:userInfos];
+	}
+}
+
+- (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName {
+	if ([elementName isEqualToString:@"account"]) {
+		[results addObject:userInfos];
+		[userInfos release];
+		userInfos = nil;
+	}
+	else if ([elementName isEqualToString:@"userid"] || [elementName isEqualToString:@"alias"] || 
+			 [elementName isEqualToString:@"pro"] || [elementName isEqualToString:@"dateformat"] || 
+			 [elementName isEqualToString:@"timezone"] || [elementName isEqualToString:@"hidemonths"] || 
+			 [elementName isEqualToString:@"hotlistpriority"] || [elementName isEqualToString:@"hotlistduedate"] || 
+			 [elementName isEqualToString:@"lastaddedit"] || [elementName isEqualToString:@"lastdelete"] || 
+			 [elementName isEqualToString:@"lastfolderedit"] || [elementName isEqualToString:@"lastcontextedit"] || 
+			 [elementName isEqualToString:@"lastgoaledit"]) {
+		[userInfos setValue:currentString forKey:elementName];
+	}
+	
+	[currentString release];
+	currentString = nil;
+}
+
+@end
