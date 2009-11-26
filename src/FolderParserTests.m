9dc0356e45907ebe6e6534901e01119614273e62
alexleutgoeb <privat@alexleutgoeb.com>
2009-11-26 11:39:56 +0100

    Added Folder parser tests, deleted xml test files.

--- src/FolderParserTests.m
+++ src/FolderParserTests.m
@@ -20,7 +20,9 @@
 	TDFoldersParser *folderParser;
 }
 
+- (void)testAddFolderWithInvalidKey;
 - (void)testAddFolderWithMissingTitle;
+- (void)testAddFolderSuccess;
 
 @end
 
@@ -39,10 +41,52 @@
 }
 
 // Tests xml of adding a folder with an empty title
-- (void)testAddFolderWithMissingTitle {
+- (void)testAddFolderWithInvalidKey {
+	
+	NSData *returnData = [@"<?xml version=\"1.0\" encoding=\"UTF-8\"?><error>key did not validate</error>" dataUsingEncoding:NSUTF8StringEncoding];
+	
 	simpleParser.tagName = @"added";
-	simpleParser.data = [NSData data];
-	// TODO: set data, test return value
+	simpleParser.data = returnData;
+	
+	NSError *error = nil;
+	NSArray *results = [simpleParser parseResults:&error];
+	
+	STAssertTrue(results != nil, @"Parser return value must not be nil.");
+	STAssertTrue([results count] == 0, @"Parser return array must be empty.");
+}
+
+// Tests xml of adding a folder with an empty title
+- (void)testAddFolderWithMissingTitle {
+	
+	NSData *returnData = [@"<?xml version=\"1.0\" encoding=\"UTF-8\"?><error>You must enter a title</error>" dataUsingEncoding:NSUTF8StringEncoding];
+	
+	simpleParser.tagName = @"added";
+	simpleParser.data = returnData;
+	
+	NSError *error = nil;
+	NSArray *results = [simpleParser parseResults:&error];
+	
+	STAssertTrue(results != nil, @"Parser return value must not be nil.");
+	STAssertTrue([results count] == 0, @"Parser return array must be empty.");
+}
+
+// Tests xml of adding a folder
+- (void)testAddFolderSuccess {
+	
+	NSString *newFolderId = @"3283361";
+	
+	NSData *returnData = [[NSString stringWithFormat:@"<?xml version=\"1.0\" encoding=\"UTF-8\"?>		<added>%@</added>", newFolderId] dataUsingEncoding:NSUTF8StringEncoding];
+	
+	simpleParser.tagName = @"added";
+	simpleParser.data = returnData;
+	
+	NSError *error = nil;
+	NSArray *results = [simpleParser parseResults:&error];
+	
+	STAssertTrue(results != nil, @"Parser return value must not be nil.");
+	STAssertTrue(error == nil, @"Parser error object must be nil.");
+	STAssertTrue([results count] == 1, @"Parser return array count must be 1.");
+	STAssertTrue([[results objectAtIndex:0] isEqualToString:newFolderId], @"New folder id must match refernce id.");
 }
 
 @end
